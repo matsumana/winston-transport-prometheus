f64c752c4fc2f9fe405c200488ed8b9a9cd0fede
Manabu Matsuzaki <matsumana@gmail.com>
2021-04-26 02:33:33 +0900

    add test

--- test/prometheusTransport.test.ts
+++ test/prometheusTransport.test.ts
@@ -1,12 +1,19 @@
+import * as winston from 'winston';
 import { Counter, register } from 'prom-client';
 import { PrometheusTransport } from '../lib';
 
+let sut: PrometheusTransport;
+
+beforeEach(() => {
+    const metric = register.getSingleMetric('winston_events_total');
+    if (metric) {
+        register.removeSingleMetric('winston_events_total');
+    }
+    sut = new PrometheusTransport();
+});
+
 describe('PrometheusTransport', () => {
-    const sut = new PrometheusTransport();
-
     it('call log() with debug, one time', async () => {
-        register.resetMetrics();
-
         sut.log({ level: 'debug', message: 'foo' }, () => {
             // do nothing
         });
@@ -25,8 +32,6 @@ describe('PrometheusTransport', () => {
     });
 
     it('call log() with error, multiple times', async () => {
-        register.resetMetrics();
-
         for (let i = 0; i < 5; i++) {
             sut.log({ level: 'error', message: 'foo' }, () => {
                 // do nothing
@@ -47,8 +52,6 @@ describe('PrometheusTransport', () => {
     });
 
     it('call log() multiple log levels', async () => {
-        register.resetMetrics();
-
         sut.log({ level: 'debug', message: 'foo' }, () => {
             // do nothing
         });
@@ -76,6 +79,42 @@ describe('PrometheusTransport', () => {
         const info = metrics.values[1];
         expect(info.labels).toEqual({ level: 'info' });
         expect(info.value).toBe(1);
+
+        const error = metrics.values[2];
+        expect(error.labels).toEqual({ level: 'error' });
+        expect(error.value).toBe(3);
+    });
+
+    it('call log() with winston Logger', async () => {
+        const logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.simple(),
+            transports: [new winston.transports.Console(), sut],
+        });
+
+        logger.debug('foo'); // since level is info (see above), will be excluded
+        logger.info('bar');
+        logger.warn('baz');
+        logger.log('warn', 'qux');
+        logger.error('quux');
+        logger.error('corge');
+        logger.log('error', 'grault');
+
+        const actual = register.getSingleMetric('winston_events_total');
+        expect(actual).toBeInstanceOf(Counter);
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const metrics = await actual.get();
+        expect(metrics.values.length).toBe(3);
+
+        const info = metrics.values[0];
+        expect(info.labels).toEqual({ level: 'info' });
+        expect(info.value).toBe(1);
+
+        const warn = metrics.values[1];
+        expect(warn.labels).toEqual({ level: 'warn' });
+        expect(warn.value).toBe(2);
 
         const error = metrics.values[2];
         expect(error.labels).toEqual({ level: 'error' });
